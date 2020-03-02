59a8be9c8c85adc7a8833d44540c56815942df9b
JaspinderPawar <jaspinder@ucreate.co.in>
2020-03-02 12:18:31 +0530

    Get connectionstring from env variable

--- src/infra/sequelize/config.js
+++ src/infra/sequelize/config.js
@@ -7,8 +7,7 @@ const env = process.env.NODE_ENV || 'development';
 
 const config = {
   development: {
-    url:
-      'postgres://qlusiissztrnvu:7ff1143faf77f1549c0fa1a4e056ad2d55e87758500cde2a5b732800e8d0daa8@ec2-46-137-188-105.eu-west-1.compute.amazonaws.com:5432/df7d5qoflm41sc',
+    url:process.env.DATABASE_URL,
     sync: false,
     logging: console.log(), // eslint-disable-line no-console,
     ssl: true,
@@ -19,7 +18,7 @@ const config = {
     reportErrorRequest: true,
   },
   test: {
-    url: process.env.DATABASE_URL,
+    url: process.env.TEST_DATABASE_URL,
     sync: process.env.SYNC_DATABASE || false,
     logging: false,
     ssl: false,
@@ -28,8 +27,7 @@ const config = {
     },
   },
   production: {
-    url:
-      'postgres://qlusiissztrnvu:7ff1143faf77f1549c0fa1a4e056ad2d55e87758500cde2a5b732800e8d0daa8@ec2-46-137-188-105.eu-west-1.compute.amazonaws.com:5432/df7d5qoflm41sc',
+    url:process.env.DATABASE_URL,
     sync: false,
     logging: false,
     ssl: true,
