a4d2a5c9dfb4f0df4a983e5efe0c92006eb45ac6
JaspinderPawar <jaspinder@ucreate.co.in>
2020-03-11 15:31:49 +0530

    Use dockrized pgadmin

--- src/infra/sequelize/config.js
+++ src/infra/sequelize/config.js
@@ -1,13 +1,19 @@
 require('dotenv').config; // eslint-disable-line no-unused-expressions
 const yn = require('yn');
+const highlightSql = require('sequelize-log-syntax-colors');
+const { format } = require('sql-formatter');
 
 const env = process.env.NODE_ENV || 'development';
+const DOCKER_DEV_DATABASE_URL =
+  'postgres://postgres:postgres@localhost:5434/dev';
+const DOCKER_TEST_DATABASE_URL =
+  'postgres://postgres:postgres@localhost:5433/test';
 
 const config = {
   development: {
-    url: process.env.DATABASE_URL,
+    url: process.env.DATABASE_URL || DOCKER_DEV_DATABASE_URL,
     sync: false,
-    logging: console.log(), // eslint-disable-line no-console,
+    logging: text => console.log(highlightSql(format(text))), // eslint-disable-line no-console,
     ssl: true,
     dialectOptions: {
       ssl: yn(process.env.DATABASE_SSL),
@@ -16,7 +22,7 @@ const config = {
     reportErrorRequest: true,
   },
   test: {
-    url: process.env.TEST_DATABASE_URL,
+    url: process.env.TEST_DATABASE_URL || DOCKER_TEST_DATABASE_URL,
     sync: process.env.SYNC_DATABASE || false,
     logging: false,
     ssl: false,
